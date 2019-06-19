90100c9b0b6d2d3ca583c0a1d094587b6b8c553a
yenniferTobon <ymtobony@unal.edu.co>
2019-06-19 22:57:42 +0000

    correction exercise 4-2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main- only print one messaje
-* return: 0
-*/
+ * main - This is a description
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
