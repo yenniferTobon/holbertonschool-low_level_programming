498b583a588152462c39b6a14a2c4b7e680df44d
yenniferTobon <ymtobony@unal.edu.co>
2019-06-20 20:43:13 +0000

    correction exercise 1.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 		printf("is %d and is 0\n", n);
 	}
 	else
-	printf("is %d and is less than 6 and not 0\n",n);
+	printf("is %d and is less than 6 and not 0\n", n);
 	return (0);
 }
