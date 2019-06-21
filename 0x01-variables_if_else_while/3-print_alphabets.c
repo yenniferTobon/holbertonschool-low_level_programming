e8554ad1592bd4dd0f487ef1ac913a01fc8d28e4
yenniferTobon <ymtobony@unal.edu.co>
2019-06-21 17:16:36 +0000

    exercise 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
