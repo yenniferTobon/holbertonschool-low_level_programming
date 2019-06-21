a1c21a7b8c4bd57c0254f09c4f24e06e57cc983c
yenniferTobon <ymtobony@unal.edu.co>
2019-06-21 02:50:41 +0000

    exercise 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -
+ * Return: 0
+ */
+int main(void)
+{	
+	return (0);
+}
