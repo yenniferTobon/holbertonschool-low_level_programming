4b62a34488b26308ab14256127a13ef57c16a2c9
yenniferTobon <ymtobony@unal.edu.co>
2019-06-21 03:38:06 +0000

    exercise 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 /**
- * main -
+ * main - print all numbers of base 10 with putchar
  * Return: 0
  */
 int main(void)
-{	
+{
+	char i = 49;
+
+	for (i = 49; i < 57; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
 	return (0);
 }
