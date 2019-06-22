41d912fa40555bd683ecfc23a662b759d8a62179
yenniferTobon <ymtobony@unal.edu.co>
2019-06-22 00:25:30 +0000

    correction exercise 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,9 +4,19 @@
  * Return: 0
  */
 
-int void(main)
+int main(void)
 {
-	
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
 	return (0);
 }
 
