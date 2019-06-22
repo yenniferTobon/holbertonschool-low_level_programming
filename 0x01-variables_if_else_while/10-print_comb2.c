dceb410ee7e52cc0b535fb4128793ba7d4a35055
yenniferTobon <ymtobony@unal.edu.co>
2019-06-22 00:10:11 +0000

    correction exercise 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	char i = '0';
-	char j = '0';
+	int  i = 48;
+	int  j = 48;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 48; j < 58; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i == 9 && j == 9)
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
 			{
 				break;
 			}
