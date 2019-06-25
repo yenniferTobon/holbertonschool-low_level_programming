d0ad2ab7df06209505fcab851c90bfbbe79814be
yenniferTobon <ymtobony@unal.edu.co>
2019-06-25 20:06:33 +0000

    exercise 3

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int c)
+{
+	int  result = 0;
+
+	result = c % 10;
+
+	if (result < 0)
+	{
+		result = -result;
+	}
+	_putchar(result + 48);
+	return (result);
+}
