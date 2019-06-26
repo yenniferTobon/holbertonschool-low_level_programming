5a0ea3a49e5f2954bec7789bfcbb51034cf13bc1
yenniferTobon <ymtobony@unal.edu.co>
2019-06-26 17:53:49 +0000

    exercise 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,2 +1,14 @@
 #include "holberton.h"
 
+/**
+ * main - test fuction that print it integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	num = 0;
+	positive_or_negative(num);
+	return (0);
+}
