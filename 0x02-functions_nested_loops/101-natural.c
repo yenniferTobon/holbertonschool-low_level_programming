48bff3f4753140df7fa2de224c99cddd660ce79d
yenniferTobon <ymtobony@unal.edu.co>
2019-06-27 01:15:31 +0000

    exercise 13-1

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,14 @@
 void main(void)
 {
 	int i = 0;
+
 	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
-		}
 	}
+
 	printf("%d\n", sum);
 }
