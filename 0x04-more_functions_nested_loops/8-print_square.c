4c61476c6d52d2c62dc8a7edc39cc57a42cdb714
yenniferTobon <ymtobony@unal.edu.co>
2019-06-28 01:44:29 +0000

    exercise 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+void print_square(int size)
+{
+	int i, m;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (m = 1; m <= size + 1; m++)
+			{
+				if (m < size + 1)
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
