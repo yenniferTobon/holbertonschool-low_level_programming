d98f0568361660c3d7705fc4759093cc13d00e48
yenniferTobon <ymtobony@unal.edu.co>
2019-07-02 18:55:55 +0000

    exercise 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,4 +7,28 @@
 
 void puts_half(char *str)
 {
+	int i = 0, tamano = 0;
+
+	while (i != -1)
+	{
+		if (*(str + i) != '\0')
+		{
+			tamano = tamano + 1;
+		}
+		else
+		{
+			i = -1;
+			break;
+		}
+		i = i + 1;
+	}
+
+	i = tamano / 2;
+
+	while (i <= tamano -1 )
+	{
+		_putchar(*(str + i));
+		i = i + 1;
+	}
+	_putchar('\n');
 }
