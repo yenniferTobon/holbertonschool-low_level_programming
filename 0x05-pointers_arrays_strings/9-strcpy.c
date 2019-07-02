c10ca1a0c30bb2792fd9ecddcf81c9b771edb26d
yenniferTobon <ymtobony@unal.edu.co>
2019-07-02 21:44:41 +0000

    exercise 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,24 @@
 
 /**
  * *_strcpy - copies the string pointed to by src
- * @s: char input
+ * @dest: char destination
+ * @src: char origin
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0, j = 0;
+
+	while (*(src + i) != '\0')
+		i = i + 1;
+
+	dest[i] = '\0';
+
+	for (j = 0; j < i; j++)
+	{
+		*(dest + j) = *(src + j);
+	}
+	return (dest);
 }
+
