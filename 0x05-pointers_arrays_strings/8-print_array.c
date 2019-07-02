9b32349ebb3b181215f217afbc5a2b19885dd3c7
yenniferTobon <ymtobony@unal.edu.co>
2019-07-02 20:49:25 +0000

    exercise 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array of integers
@@ -7,4 +8,14 @@
 
 void print_array(int *a, int n)
 {
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+	}
+	printf("\n");
 }
