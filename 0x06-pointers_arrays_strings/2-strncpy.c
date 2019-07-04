8feb22f4ea3e08c1db5f2455197c6451a59d0aa3
yenniferTobon <ymtobony@unal.edu.co>
2019-07-04 19:10:54 +0000

    exercise 1, 2, 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,43 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: number int input
+ * *_strncpy - concatenates two strings
+ * @dest: char dest
+ * @src: char a appends
+ * @n: use n bytes to src
+ * Return: char dest
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int tamanoDest = 0, l = 0, tamanoSrc = 0, i = 0;
+
+	while (*(dest + tamanoDest) != '\0')
+	{
+		tamanoDest = tamanoDest + 1;
+	}
+
+	while (*(src + tamanoSrc) != '\0')
+	{
+		tamanoSrc = tamanoSrc + 1;
+	}
+
+	if (tamanoSrc <= n)
+	{
+		for (i = 0; i < n; i++)
+		{
+			*(dest + i) = '\0';
+		}
+	}
+
+	for (l = 0; src[l] != '\0'; l++)
+	{
+		if (l < n)
+		{
+			*(dest + l) = *(src + l);
+		}
+		else
+			break;
+	}
+	return (dest);
+}
