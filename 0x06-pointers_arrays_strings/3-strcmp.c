ec4fbf457ef05084980a55872ff6d81cc2430635
yenniferTobon <ymtobony@unal.edu.co>
2019-07-04 21:27:52 +0000

    exercise 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,24 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: number int input
+ * _strcmp - compares two strings.
+ * @s1: number int input
+ * @s2:
+ * Return:
  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i= 0, diferencia = 0;
+
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		if ((s1[i]) - (s2[i]) != 0)
+		{
+			diferencia = (s1[i]) - (s2[i]);
+			break;
+		}
+	}
+	return (diferencia);
+}
 
