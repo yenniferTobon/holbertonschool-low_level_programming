8d2a98e885165f512d7560011a67323a39984678
yenniferTobon <ymtobony@unal.edu.co>
2019-07-04 22:36:59 +0000

    exercise 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,24 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: number int input
+ * reverse_array - reverses an array of integers
+ * @n: array a change
+ * @a: num elements of the array
  */
 
+void reverse_array(int *a, int n)
+{
+	int aux[n];
+	int i = 0, j = 0, l = n;
+
+	for (i = 0; i < n; i++)
+	{
+		l = l - 1;
+		aux[i] = *(a + l);
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		*(a + j) = aux[j];
+	}
+}
