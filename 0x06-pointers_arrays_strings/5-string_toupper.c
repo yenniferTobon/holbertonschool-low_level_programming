fcb2da545d8417d98d39a71b178aafa0c761efec
yenniferTobon <ymtobony@unal.edu.co>
2019-07-05 00:17:22 +0000

    exercise 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,32 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: number int input
+ * *string_toupper - all lowercase of a string to uppercase
+ * @a: char in lowercase
+ * Return: char
  */
 
+char *string_toupper(char *a)
+{
+	int tamano = 0, i = 0;
+	char aux[500];
+
+	while (*(a + tamano) != '\0')
+	{
+		tamano = tamano + 1;
+	}
+
+	for (i = 0; i < tamano; i++)
+	{
+		if (*(a + i) <= 90)
+		{
+			*(a + i) = *(a + i);
+		}
+		else
+		{
+			aux[i] = (*(a + i)) - 32;
+			*(a + i) = aux[i];
+		}
+	}
+	return (a);
+}
