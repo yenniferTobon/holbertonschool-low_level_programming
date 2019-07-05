9f4d57f2a248f22db003505b30715741169bbcca
yenniferTobon <ymtobony@unal.edu.co>
2019-07-05 04:40:49 +0000

    Exercise 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
  * *cap_string - capitalizes all words of a string
  * @a: char in lowercase
@@ -8,24 +9,31 @@
 
 char *cap_string(char *a)
 {
-	int tamano = 0, i = 0;
-	char aux[500];
+	int tamano = 0, i = 0, j = 0;
+	int vector[13] = {9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 125, 123, 32};
 
 	while (*(a + tamano) != '\0')
 	{
 		tamano = tamano + 1;
 	}
 
+
 	for (i = 0; i < tamano; i++)
 	{
-		if (*(a + i) <= 90)
+		for (j = 0; j <= 12; j++)
 		{
-			*(a + i) = *(a + i);
-		}
-		else
-		{
-			aux[i] = (*(a + i)) - 32;
-			*(a + i) = aux[i];
+			if (*(a + i) == vector[j])
+			{
+				int i2 = i + 1;
+
+				if ((*(a + i2) >= 65 && *(a + i2) <= 90) || (*(a + i2) >= 97 && *(a + i2) <= 122))
+				{
+					if (*(a + i2) >= 97 && *(a + i2) <= 122)
+					{
+						*(a + i2) = *(a + i2) - 32;
+					}
+				}
+			}
 		}
 	}
 	return (a);
