b4d8c6a1a81bb61010f9ed5fa814b34d08179127
yenniferTobon <ymtobony@unal.edu.co>
2019-07-05 03:15:37 +0000

    Exercise 8

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,34 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: number int input
+ * *leet - encodes a string
+ * @a: input chat
+ * Return: char
  */
 
+char *leet(char *)
+{
+	int i = 0, j = 0;
+
+	char matrix[][] = {{ 'a' , 'A' },
+			{ 'e' , 'E' },
+			{ 'o' , 'O' },
+			{ 't' , 'T' },
+			{ 'l' , 'L' }};
+	int vector[5] = { '4' , '3' , '0' , '7' , '1' };
+
+	while (s[i] != 0)
+	{
+		char texto = s[i];
+
+		for (j = 0; j < 5; j++)
+		{
+			if (texto == matrix[j][0] || texto == matrix[j][1])
+			{
+				s[i] = texto[i];
+			}
+		}
+		i = i + 1;
+	}
+	return (s);
+}
