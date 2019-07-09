401baca7fa71ddd88a6644e55d3138b20808fab3
yenniferTobon <ymtobony@unal.edu.co>
2019-07-09 02:25:27 +0000

    Exercise 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,3 +8,20 @@
  */
 
 char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		int i = 0;
+
+		while (*(accept + i) != '\0')
+		{
+			if (*(s) == *(accept + i))
+			{
+				return (s);
+			}
+			i++;
+		}
+		s++;
+	}
+	return (s);
+}
