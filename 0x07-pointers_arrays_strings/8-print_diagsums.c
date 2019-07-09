8aa1946514accb41c1014f95eff36639b5292652
yenniferTobon <ymtobony@unal.edu.co>
2019-07-09 00:36:04 +0000

    Exercise 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_diagsums - function that prints the sum of the two diagonals of a square matrix of integers
+ * @a: number input
+ * @size: zise input
  */
 
 void print_diagsums(int *a, int size)
--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- *
+ * set_string - function that sets the value of a pointer to a char
+ * @s: char to input
+ * @to: char to input
  */
 
 void set_string(char **s, char *to)
