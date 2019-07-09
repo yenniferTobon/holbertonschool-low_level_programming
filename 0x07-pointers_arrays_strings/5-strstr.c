2f30fd3b0cdd9bd6cc13fc5cbc8e201ccd1d2def
yenniferTobon <ymtobony@unal.edu.co>
2019-07-09 02:57:33 +0000

    Exercise 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	while (*needle != '\0')
+	{
+		int i = 0;
+
+		while (*(haystack + i) != '\0')
+		{
+			if (*(needle) == *(haystack + i))
+			{
+				return (needle);
+			}
+			i++;
+		}
+		needle++;
+	}
+	if (*needle == *haystack)
+		return ('\0');
+
+	return ('\0');
 }
