ede873ffb1d5452332a05e696fe57f283b9c01f8
yenniferTobon <ymtobony@unal.edu.co>
2019-07-09 04:05:58 +0000

    Exercise 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - function that prints the sum of the two diagonals of a square matrix of integers
@@ -8,4 +9,19 @@
 
 void print_diagsums(int *a, int size)
 {
+	int i = 0, sumaM1 = 0, sumaM2 = 0;
+
+	while (i < (size * size))
+	{
+		sumaM1 = sumaM1 + a[i];
+		i = i + size + 1;
+	}
+	i = size -1;
+	while (i < (size * size) - 1)
+        {
+                sumaM2 = sumaM2 + a[i];
+                i = i + size - 1;
+        }
+	printf("%d, %d", sumaM1, sumaM2);
+	printf("\n");
 }
