9477918ce37a76f03b412168bd0e6b4cd3828410
yenniferTobon <ymtobony@unal.edu.co>
2019-07-11 03:28:33 +0000

    Exercise 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "holberton.h"
 
 /**
+* funsqrt - function recursive that return natural square
+* @n: number original input
+* @m: number constant input
+* Return: int square
+*/
+
+int funsqrt(int n, int m)
+{
+	if ((m * m) == n)
+	{
+		return (m);
+	}
+	else if ((m * m) < n)
+	{
+		return (funsqrt(n, m + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: number input
  * Return: int
@@ -8,4 +31,8 @@
 
 int _sqrt_recursion(int n)
 {
+	if (n <= 0)
+		return (-1);
+	else
+		return (funsqrt(n, 1));
 }
