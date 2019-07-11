d5d43765ee7d571fb651fbe907ee6ecc38dbcc2c
yenniferTobon <ymtobony@unal.edu.co>
2019-07-11 01:22:46 +0000

    Exercise 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,32 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - function that returns 1 if the input integer is a prime number
- * @n: number input
- *
- */
+* funPrimo - Checks if number has divisors different from 1 and n.
+* @d: Divisor
+* @n: Number
+* Return: int
+*/
+
+int funPrimo(int d, int n)
+{
+	if (n % d == 0)
+		return (0);
+	else if (n / 2 > d)
+		return (funPrimo(d + 2, n));
+	else
+		return (1);
+}
+
+/**
+* is_prime_number - Checks if the input integer is a prime number.
+* @n: Number to check.
+* Return: int
+*/
 
 int is_prime_number(int n)
 {
+	if ((!(n % 2) && n != 2) || n < 2)
+		return (0);
+	else
+		return (funPrimo(3, n));
 }
