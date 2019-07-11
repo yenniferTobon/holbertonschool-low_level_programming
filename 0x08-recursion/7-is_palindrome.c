8bdcbd0130274cb2e655b160045731256b8d3bd8
yenniferTobon <ymtobony@unal.edu.co>
2019-07-11 01:30:47 +0000

    Exercise 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,12 +1,51 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - function that returns 1 if a string is a palindrome and 0 if not
- * @s: char
- * Return: int
+ * _strlen_recursion - returns the length of a string
+ * @s: array char
+ * Return: suma
+ */
+
+int _strlen_recursion(char *s)
+{
+	int suma = 0;
+
+	if (*s != '\0')
+	{
+		suma = 1 + _strlen_recursion(s + 1);
+	}
+	return (suma);
+}
+
+/**
+ * funIsPalim - Checks characters of palindrome.
+ * @s: String to check.
+ * @size: Size of string.
+ * @c: Index.
+ * Return: int 1 if palindrome, 0 if not.
+ */
+
+int funIsPalim(char *s, int size, int c)
+{
+	if (c >= size / 2)
+		return (1);
+	else if (s[c] == s[size - c - 1])
+		return (funIsPalim(s, size, c + 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: String to check.
+ * Return: 1 if a string is a palindrome and 0 if not.
  */
 
 int is_palindrome(char *s)
 {
+	int size = _strlen_recursion(s);
+	int c = 0;
+
+	return (funIsPalim(s, size, c));
 }
 
