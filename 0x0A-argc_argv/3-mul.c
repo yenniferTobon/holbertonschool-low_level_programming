118a48fd45bd42276cab6d93fb28816405c6f0f7
yenniferTobon <ymtobony@unal.edu.co>
2019-07-13 20:49:40 +0000

    Exercise 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - function that multiplies two numbers
  * @argc: number arguments input
@@ -7,6 +7,19 @@
  * Return: int result of the multiplication
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
+	int mult = 0, num1 = 0, num2 = 0;
+
+	if (argc < 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mult = (num1 * num2);
+	printf("%d\n", mult);
+	return (1);
 }
