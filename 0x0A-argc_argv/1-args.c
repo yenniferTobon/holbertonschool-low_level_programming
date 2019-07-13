a548aad02f58886f9331ada70cc8e8ff230d836f
yenniferTobon <ymtobony@unal.edu.co>
2019-07-13 19:46:16 +0000

    exercise 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,14 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - function that of prints its name
+ * @argc: nothing
+ * @argv: char name
+ * Return: int 0 is success exit
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
