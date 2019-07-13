02008174bb205baa1be11e8b88dff19c2bb84076
yenniferTobon <ymtobony@unal.edu.co>
2019-07-13 19:32:30 +0000

    folder and exercise 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(int argc __attribute__((unused)), char* argv[])
+/**
+ * main - function that of prints its name
+ * @argc: nothing
+ * @argv: char name
+ * Return: int 0 is success exit
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
