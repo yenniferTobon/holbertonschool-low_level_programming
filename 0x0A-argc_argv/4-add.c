5788bf5f086a4f612aa9cdfab89e67cf5428a26c
yenniferTobon <ymtobony@unal.edu.co>
2019-07-14 14:46:17 +0000

    Exercise 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; **(argv + 1) != '\0'; j++)
+		for (j = 0; *(*(argv + i) + j) != '\0'; j++)
 		{
 			if (**(argv + i) < 48 || **(argv + i) > 57)
 			{
