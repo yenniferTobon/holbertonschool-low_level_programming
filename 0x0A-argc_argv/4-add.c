2b094bd222f110af184b8835f3b765d51e563a1f
yenniferTobon <ymtobony@unal.edu.co>
2019-07-14 14:37:25 +0000

    Exercise 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,21 @@ int main(int argc, char *argv[])
 {
 	int i = 1, suma = 0, num1 = 0;
 
-	if (argc < 2 || argv[1] == '\0')
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
 	while (i <= (argc - 1))
 	{
-		if (**(argv + i) < 48 || **(argv + i) > 57)
+		for (j = 0; **(argv + 1) != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (**(argv + i) < 48 || **(argv + i) > 57)
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
-		else
-		{
-			num1 = atoi(argv[i]);
-			suma = suma + num1;
-		}
+		num1 = atoi(argv[i]);
+		suma = suma + num1;
 		i++;
 	}
 	printf("%d\n", suma);
-	return (0);
+	return (suma);
 }
 
