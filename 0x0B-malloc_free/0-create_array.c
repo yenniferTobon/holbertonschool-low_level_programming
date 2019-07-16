b47db4e7b61594990fc30fa47b1169b252baf4b4
yenniferTobon <ymtobony@unal.edu.co>
2019-07-16 19:19:26 +0000

    exercise 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 /**
  * *create_array - array of chars, and initializes it with a specific char
- * @size:
- * @c:
+ * @size: array size
+ * @c: char to repeat
+ * Return: char p
  */
 
 char *create_array(unsigned int size, char c)
 {
- 	char *p;
+	char *p;
 	unsigned int i = 0;
+
+
+	if (size == 0)
+		return (NULL);
 
 	p = malloc(sizeof(char) * size);
 
-	if (size == 0)
-		return (NULL)
-;
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
