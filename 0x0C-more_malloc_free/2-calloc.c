a2b67d1332f645ffd888a00300f213dc30363101
yenniferTobon <ymtobony@unal.edu.co>
2019-07-18 19:44:54 +0000

    exercise 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,32 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <limits.h>
+#include <stdio.h>
 /**
- * *malloc_checked - allocates memory using
- * @b: value assign memory
- * Return: void
+ * *_calloc - allocates memory for an array
+ * @nmemb: size column
+ * @size: size fila
+ * Return: pointer
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *resp;
+	unsigned int i = 0, j = 0, sizeMem = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	for (i = 1; i <= nmemb; i++)
+	{
+		for (j = 1; j <= size; j++)
+			sizeMem = sizeMem + j;
+	}
+
+	resp = malloc(sizeof(int) * sizeMem);
+
+	if (resp == NULL)
+		return (NULL);
+
+	return (resp);
+}
