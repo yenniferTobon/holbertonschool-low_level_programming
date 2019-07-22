2cd800bb91d6934a120d2bcfe4620a8b2ff7b103
yenniferTobon <ymtobony@unal.edu.co>
2019-07-22 19:55:14 +0000

    struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Short description
+ * @name: Name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: date dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
