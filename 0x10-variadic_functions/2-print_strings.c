7897f0651ab62ba3f7674cd360d249aae9b79024
yenniferTobon <ymtobony@unal.edu.co>
2019-07-26 20:23:11 +0000

    function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,14 +12,28 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list arguments;
+
+	char *palabra;
+
 	va_start(arguments, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(arguments, char));
-		if (i != (n-1))
-			printf("%s", separator);
+		palabra = va_arg(arguments, char*);
+		if (palabra == NULL)
+		{
+			printf("%s", "nil");
+		}
+		else
+			printf("%s", palabra);
+
+		if (separator != NULL)
+		{
+			if (i != (n - 1))
+				printf("%s", separator);
+		}
 	}
 	va_end(arguments);
+
 	printf("\n");
 }
