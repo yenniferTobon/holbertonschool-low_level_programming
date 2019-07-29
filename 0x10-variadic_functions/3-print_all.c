8d6dda5748a036ed9beb89967fa664c3881d0ff2
yenniferTobon <ymtobony@unal.edu.co>
2019-07-29 03:21:20 +0000

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,22 +1,47 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
+/**
+ * funt_valido - function that print valido
+ * @format: input
+ * @i: posicion
+ * Return: 1 - OK, 0 fail
+ **/
+int funt_valido(const char * const format, int i)
+{
+	int j = i + 1, sw = 0;
+
+	while (format[j] != '\0')
+	{
+		if ((format[i] ==  'c' ||
+		format[i] == 'i' || format[i] == 'f' || format[i] == 's') &&
+		(format[j] == 'c' || format[j] == 'i' || format[j] == 'f' ||
+		format[j] == 's'))
+		{
+			sw = 1;
+			break;
+		}
+		j++;
+	}
+	return (sw);
+}
+
+
 /**
  * print_all - function that prints anything.
- * @format:
+ * @format: input
  **/
-
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int j = 0;
-	va_list arguments;
-	va_start(arguments, format);
+	int i = 0, sw = 0;
 	char *palabra;
+	va_list arguments;
 
-	while(format[i] != '\0')
+	va_start(arguments, format);
+	while (format[i] != '\0')
 	{
-		switch(format[i])
+		switch (format[i])
 		{
 			case 'c':
 				printf("%c", (char)va_arg(arguments, int));
@@ -34,21 +59,8 @@ void print_all(const char * const format, ...)
 				printf("%s", palabra);
 				break;
 		}
-		j = i + 1;
-		int sw = 0;
-
-		while(format[j] != '\0')
-		{
-			if ((format[i] ==  'c' ||
-			format[i] == 'i' || format[i] == 'f' || format[i] == 's') &&
-			(format[j] == 'c' || format[j] == 'i' || format[j] == 'f' || format[j] == 's'))
-			{
-				sw = 1;
-				break;
-			}
-			j++;
-		}
-		switch(sw)
+		sw = funt_valido(format, i);
+		switch (sw)
 		{
 			case 1:
 				printf(", ");
