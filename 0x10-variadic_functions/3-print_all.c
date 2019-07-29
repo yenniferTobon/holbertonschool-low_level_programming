ce4a7f5653cf7b1c81e1769b6f0a5b1ca6e441c1
yenniferTobon <ymtobony@unal.edu.co>
2019-07-29 03:44:06 +0000

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,73 +1,74 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
+#include "variadic_functions.h"
 
 /**
- * print_comma - Struct op
- * @format: The operatori
- * @i: counter
- *
- * Return: return func
- */
-void print_comma(const char *format, int i)
+ * funt_valido - function that print valido
+ * @format: input
+ * @i: posicion
+ * Return: 1 - OK, 0 fail
+ **/
+int funt_valido(const char * const format, int i)
 {
-int j;
+	int j = i + 1, sw = 0;
 
-j = i + 1;
-while ((format[j] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 's' || format[i] == 'f'))
-{
-	if  (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 's' || format[j] == 'f')
+	while (format[j] != '\0')
 	{
-		printf(", ");
-		break;
+		if ((format[i] ==  'c' ||
+		format[i] == 'i' || format[i] == 'f' || format[i] == 's') &&
+		(format[j] == 'c' || format[j] == 'i' || format[j] == 'f' ||
+		format[j] == 's'))
+		{
+			sw = 1;
+			break;
+		}
+		j++;
 	}
-j++;
+	return (sw);
 }
-}
+
 /**
- * print_all - Struct op
- * @format: The operator
- *
- * Return: return func
- */
+ * print_all - function that prints anything.
+ * @format: input
+ **/
 void print_all(const char * const format, ...)
 {
-int i = 0;
-char *vble;
-va_list vprint;
+	int i = 0, sw = 0;
+	char *palabra;
+	va_list arguments;
 
-va_start(vprint, format);
-while (format[i] != '\0' && format != NULL)
-{
-	switch (format[i])
-{
-		case 'c':
-			printf("%c", va_arg(vprint, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(vprint, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(vprint, double));
-			break;
-		case 's':
-			vble   = va_arg(vprint, char*);
-			if (vble == NULL)
-			{
-				printf("(nil)");
+	va_start(arguments, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arguments, int));
 				break;
-			}
-			printf("%s", vble);
-			break;
+			case 'i':
+				printf("%i", va_arg(arguments, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arguments, double));
+				break;
+			case 's':
+				palabra = va_arg(arguments, char*);
+				if (palabra == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", palabra);
+				break;
+		}
+		sw = funt_valido(format, i);
+		switch (sw)
+		{
+			case 1:
+				printf(", ");
+		}
+		i++;
+	}
+	va_end(arguments);
+	printf("\n");
 }
-print_comma(format, i);
-i++;
-}
-va_end(vprint);
-printf("\n");
-}
-
-
