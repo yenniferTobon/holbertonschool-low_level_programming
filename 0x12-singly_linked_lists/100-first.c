18226b967a186404228e41e3b579708aac897db0
yenniferTobon <ymtobony@unal.edu.co>
2019-08-02 02:19:03 +0000

    100-first.c - function that prints text before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startup - check the code for Holberton School students.
+ *
+ * Return: void
+ */
+void startup(void)
+{
+	char *temp =  "I bore my house upon my back!\n";
+
+	printf("You're beat! and yet, you must allow,\n%s", temp);
+}
