aedf22fff619bfb1f95dae79b90c118d76b76105
yenniferTobon <ymtobony@unal.edu.co>
2019-08-07 03:57:13 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,40 @@
-#include <stdlib.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: head list
- * @idx: indice
- * @n: value the n
- * Return: listint_t
- **/
-
+* insert_nodeint_at_index - Inserts a new node at a given position.
+* @head: Pointer to pointer to the linked list.
+* @idx: Index desired for the new node.
+* @n: Number to initialize the new node.
+* Return: Pointer to the node at index, or NULL if the index doesn't exist.
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux;
-	unsigned int position = 0;
+	listint_t *iterator = *head, *new;
 
-	aux = *head;
-	if (head == NULL || *head == NULL)
+	if ((!*head && idx) || !head)
+		return (0);
+
+	while (idx > 1)
 	{
-		return (NULL);
+		iterator = iterator->next;
+		if (!iterator)
+			return (0);
+		idx--;
 	}
-
-	while ((aux != NULL) && (position != (idx - 1)))
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+	if (!idx)
 	{
-		aux = (aux)->next;
-		position++;
-	}
-
-	listint_t *newN = malloc(sizeof(listint_t));
-
-	if (newN == NULL)
-	{
-		return (NULL);
-	}
-	if (idx == 0)
-	{
-		newN->next = *head;
-		*head = newN;
+		*head = new;
+		new->next = iterator;
 	}
 	else
 	{
-		newN->next = aux->next;
-		aux->next = newN;
+		new->next = iterator->next;
+		iterator->next = new;
 	}
-	newN->n = n;
-	return (newN);
+	new->n = n;
+	return (new);
 }
+
