0ac8f1caec0ec079a44392f304b8137078605ef2
yenniferTobon <ymtobony@unal.edu.co>
2019-08-07 03:52:53 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int position = 0;
 
 	aux = *head;
+	if (head == NULL || *head == NULL)
+	{
+		return (NULL);
+	}
 
 	while ((aux != NULL) && (position != (idx - 1)))
 	{
