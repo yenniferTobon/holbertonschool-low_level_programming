083930b5373a695055d35cad226077ac06c6fe11
yenniferTobon <ymtobony@unal.edu.co>
2019-08-09 03:51:01 +0000

    sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: num in int
+ * @index: position
+ * Return: int in decimal
+ **/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+	{
+		return (-1);
+	}
+	else
+	{
+		*n = *n & ~(1 << index);
+	}
+	return (1);
+}
