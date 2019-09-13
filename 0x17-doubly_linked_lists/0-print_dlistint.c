cd4115aefd4cbf7431af6c9a1235edc7b87c0705
yenniferTobon <ymtobony@unal.edu.co>
2019-09-13 19:47:09 +0000

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint: prints all the elements of a dlistint_t list.
+ * @h: head liked list
+ * Return: size_t the size linked list
+ **/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		size = size + 1;
+		h = h->next;
+	}
+	return (size);
+}
