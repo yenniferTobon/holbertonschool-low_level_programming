732a1c2bb23d989cb09d3193ca262645db4fdd5e
yenniferTobon <ymtobony@unal.edu.co>
2019-09-13 19:59:42 +0000

    1-dlistint_len.c returns the number of elements in a linked dlistint_t list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list.
+ * @h: head liked list
+ * Return: size_t the size linked list
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size;
+
+	while (h != NULL)
+	{
+		size = size + 1;
+		h = h->next;
+	}
+	return (size);
+}
