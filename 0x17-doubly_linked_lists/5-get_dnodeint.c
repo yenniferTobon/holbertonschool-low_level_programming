85aeee682f6194451e60496278a11c2fb014413c
yenniferTobon <ymtobony@unal.edu.co>
2019-09-15 19:55:00 +0000

    5-get_dnodeint.c  function that free a dlistint_t list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: head the dlistint_t
+ * @index: poticion a print
+ * Return: dlistint_t
+ **/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *aux;
+	unsigned int i = 0;
+
+	aux = head;
+
+	while (aux != NULL)
+	{
+		if (i == index)
+		{
+			return (aux);
+		}
+		else
+		{
+			if (aux->next != NULL)
+			{
+				aux = aux->next;
+			}
+		}
+		i = i + 1;
+	}
+	return (NULL);
+}
+
