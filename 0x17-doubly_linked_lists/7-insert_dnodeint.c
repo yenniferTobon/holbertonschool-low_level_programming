4bfef1c45c594b19773cba1bb0b9c2df75fe0694
yenniferTobon <ymtobony@unal.edu.co>
2019-09-16 02:59:34 +0000

    7-insert_dnodeint.c inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,44 +13,43 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0;
 	dlistint_t *aux = *h, *neW;
 
 	if ((!*h && idx) || h == NULL)
 		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (aux != NULL && j == 0)
+	while (i < idx - 1)
 	{
-		if (aux->next == NULL)
-		{
-			j = 1;
-			return (add_dnodeint_end(h, n));
-		}
-		else
-		{
-			if (i == idx - 1)
-			{
-				neW = malloc(sizeof(dlistint_t));
+		aux = aux->next;
+		if (!aux)
+			return (NULL);
+		i++;
+	}
 
-				if (neW == NULL)
-					return (NULL);
-				aux->next->prev = neW;
-				neW->next = aux->next;
-				aux->next = neW;
-				neW->prev = aux;
-				neW->n = n;
-				j = 1;
-			}
-			else
+	if (aux->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	while (aux != NULL)
+	{
+		if (i == idx - 1)
+		{
+			neW = malloc(sizeof(dlistint_t));
+			if (neW == NULL)
 			{
-				if (!aux)
-					return (NULL);
+				return (NULL);
 			}
-			i = i + 1;
-			aux = aux->next;
+			aux->next->prev = neW;
+			neW->next = aux->next;
+			aux->next = neW;
+			neW->prev = aux;
+			neW->n = n;
 		}
+		i = i + 1;
+		aux = aux->next;
 	}
 	return (neW);
 }
