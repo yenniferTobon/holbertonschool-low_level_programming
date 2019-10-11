d43886139d8c4a1ca33869996d12d2bb2a15a538
yenniferTobon <ymtobony@unal.edu.co>
2019-10-11 22:52:14 +0000

    0-hash_table_create.c function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t *) * size);
+	new_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
