4e1f0e4eadb8f5e7ef1da8e02f3b64cc92c11778
yenniferTobon <ymtobony@unal.edu.co>
2019-10-11 23:21:54 +0000

    3-hash_table_set.c function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: table ht
+ * @key: string to convert in hash
+ * @value: value to the key
+ * Return: return hash code
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int position;
+	hash_node_t *new, *list, *start;
+
+	position = key_index((const unsigned char *)key, ht->size);
+	list = ht->array[position];
+
+	if  (list == NULL)
+	{
+		new = malloc(sizeof(hash_node_t));
+		new->key = strdup(key);
+		new->value = strdup(value);
+		ht->array[position] = new;
+		return (1);
+	}
+	while (list != NULL)
+	{
+		if (strcmp(list->key, key) == 0)
+		{
+			free(list->value);
+			list->value = strdup(value);
+			return (1);
+		}
+		list = list->next;
+	}
+	start = ht->array[position];
+
+	new =  malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = start;
+	ht->array[position] = start;
+	return (1);
+}
