00ef98a8c35a84e0a701cc34c31b8f87f7253919
yenniferTobon <ymtobony@unal.edu.co>
2019-10-28 23:34:56 +0000

    0-bubble_sort.c sorts an array of integers in ascending order using the Bubble sort

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 	size_t num_Elements_Sort = 0, i = 0;
 	int item = 0;
 
-	if (array == NULL || size < 0)
+	if (array == NULL || size < 2)
 	{
 		return;
 	}
