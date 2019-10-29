51020736ab97dd68f8e4317802043fdd4ed167bf
yenniferTobon <ymtobony@unal.edu.co>
2019-10-29 00:08:03 +0000

    1-0 big O notations of the time complexity of the Insertion sort

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -9,16 +9,19 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = *list;
+	listint_t *head;
+
+	if (*list == NULL)
+	{
+		return;
+	}
 
 	if (*list != NULL && (*list)->next->next == NULL)
 	{
 		return;
 	}
-	if (*list == NULL)
-	{
-		return;
-	}
+
+	head = *list;
 	while (head != NULL)
 	{
 		while (head->prev != NULL && head->prev->n > head->n)
