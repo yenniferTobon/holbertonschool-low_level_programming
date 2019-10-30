dc80c7343e3c5f09d6c7cd1ad0d7343c323c7d3c
yenniferTobon <ymtobony@unal.edu.co>
2019-10-30 02:29:08 +0000

    2-selection_sort.c sorts an array of integers in ascending order using the Selection sort

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts array of integers, ascending order - Selection sort
+ * @array: input array
+ * @size: size array
+ *
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	int minimum, temp = 0, aux = 0;
+	size_t i = 0, position;
+
+	for (position = 0; position < size; position++)
+	{
+		minimum = array[position];
+
+		for (i = position + 1; i < size; i++)
+		{
+			if (array[i] <= minimum)
+			{
+				minimum = array[i];
+				aux = i;
+			}
+		}
+		if (minimum != array[position])
+		{
+			temp = array[position];
+			array[position] = minimum;
+			array[aux] = temp;
+			print_array(array, size);
+		}
+	}
+
+}
