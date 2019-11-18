d3a0fd29b62308cd26e575c76c1b91a7b1d3feb3
yenniferTobon <ymtobony@unal.edu.co>
2019-11-18 01:48:13 +0000

    5-island_perimeter.py - function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """ returns the perimeter """
+
+    perimeter = 0
+    len_row = len(grid)
+    len_column = len(grid[0])
+    for row in range(len_row):
+        for column in range(len_column):
+            if grid[row][column] == 1:
+                if grid[row][column - 1] == 0 or column == 0:
+                    perimeter += 1
+                if len_row == column:
+                    perimeter += 1
+                elif grid[row][column + 1] == 0:
+                    perimeter += 1
+                if grid[row - 1][column] == 0 or row == 0:
+                    perimeter += 1
+                if len_column == row:
+                    perimeter += 1
+                elif grid[row + 1][column] == 0:
+                    perimeter += 1
+    return perimeter
