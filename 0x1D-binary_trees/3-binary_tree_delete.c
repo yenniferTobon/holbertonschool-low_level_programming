2c8c56cb6af03f67dd896f82230533c5e654ba6e
yenniferTobon <ymtobony@unal.edu.co>
2019-12-04 16:41:46 -0500

    3-binary_tree_delete.c function that deletes an entire binary tree

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -13,13 +13,13 @@ void binary_tree_delete(binary_tree_t *tree)
 		;
 	}
 
-	if (tree->right)
-	{
-		binary_tree_delete(tree->right);
-	}
 	if (tree->left)
 	{
 		binary_tree_delete(tree->left);
+	}
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
 	}
 	free(tree);
 }
