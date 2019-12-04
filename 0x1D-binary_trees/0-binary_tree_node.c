aaf0bf3d11ed045e6d8b45231ef47436b887eb96
yenniferTobon <ymtobony@unal.edu.co>
2019-12-04 14:44:52 -0500

    0-binary_tree_node.c Write a function that creates a binary tree node

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -12,8 +12,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newTree = malloc(sizeof(binary_tree_t));
 
 	if (newTree == NULL)
-        {
-                return (NULL);
+	{
+		return (NULL);
 	}
 
 	newTree->n = value;
