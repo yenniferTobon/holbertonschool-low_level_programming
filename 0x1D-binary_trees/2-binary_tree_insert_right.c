206968e1797f0233e66c73001369a7fe2e879b0c
yenniferTobon <ymtobony@unal.edu.co>
2019-12-04 15:41:02 -0500

    2-binary_tree_insert_right.c

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -5,7 +5,7 @@
  * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- * Return: pointer to the new node, or NULL on failure
+ * Return: pointer to the new node, or NULL on failure.
  **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
