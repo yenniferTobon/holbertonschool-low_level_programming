8a93268d5e8d650e3282f2b406cb33869459345c
yenniferTobon <ymtobony@unal.edu.co>
2019-12-05 14:02:17 -0500

    12-binary_tree_leaves.c function that counts the leaves in a binary tree

---
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree.
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0.
+ **/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t size_total = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+	{
+		size_total = binary_tree_size(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		size_total = binary_tree_size(tree->right);
+	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		size_total = size_total + 1;
+		return (size_total);
+	}
+	return (size_total);
+}
+
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0.
+ **/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size_total = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+	{
+		size_total = size_total + binary_tree_size(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		size_total = size_total + binary_tree_size(tree->right);
+	}
+	size_total = size_total + 1;
+	return (size_total);
+}
+
